0da21a5303d762c7f776d20fe8fc8460942d818e
Vanessa Bautista <vanessawebdev@gmail.com>
2022-12-02 09:20:33 -0800

    Added draggable functionality

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+const initList = [1, 2, 3, 4, 5, 6];
 
 function App() {
+  const [list, setList] = useState(initList);
+  const [draggedItem, setDraggedItem] = useState(null);
+
+  const onDragStartHandle = (e, index) => {
+    setDraggedItem(list[index]);
+    e.dataTransfer.effectAllowed = "move";
+    e.dataTransfer.setData("text/html", e.target.parentNode);
+    e.dataTransfer.setDragImage(e.target.parentNode, 20, 20);
+  };
+
+  const onDragOverHandle = (index) => {
+    const draggedOverItem = list[index];
+    if (draggedOverItem === draggedItem) {
+      return;
+    }
+
+    const items = list.filter((item) => item !== draggedItem);
+
+    items.splice(index, 0, draggedItem);
+
+    setList(items);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Drag and Drop List</h1>
+        <ul>
+          {list.map((item, idx) => {
+            return (
+              <li
+                key={idx}
+                onDragOver={() => onDragOverHandle(idx)}
+                className="item-style"
+              >
+                <div draggable onDragStart={(e) => onDragStartHandle(e, idx)}>
+                  {item}
+                </div>
+              </li>
+            );
+          })}
+        </ul>
       </header>
     </div>
   );
